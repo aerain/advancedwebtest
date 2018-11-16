22ab9a6ec4cb2b801dbd5f1b4eadb1f1eb06b38c
aerain <j40f893@gmail.com>
2018-11-16 17:59:45 +0900

    gulpconcat

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
 gulp.task('build', function() {
-    
+    return gulp.src(['src/util.js', 'src/app.js'])
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('build'));
 });
